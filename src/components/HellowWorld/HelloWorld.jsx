2936bdb2bf891f56d097e3c2f0a860fff873bc5b
Hexcrow-k17 <kimaleksander1702@gmail.com>
2024-01-26 12:10:21 +0100

    Refactor App component and add HelloWorld component

---
+++ src/components/HellowWorld/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HelloWorld() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+      <div className="bg-purple-900 p-40">
+        <h1 className="text-center text-6xl font-bold text-gray-800">*Hello Wrld*</h1>
+      </div>
+    </div>
+  );
+}
